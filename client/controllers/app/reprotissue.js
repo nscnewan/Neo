f3ba1750b109639d0c1ab71db70b036f0682d942
Neo Wang <neo.wang@flex.com>
2017-10-25 02:15:03 -0700

    [Studio] Modified screen "ReprotIssue"

--- client/controllers/app/reprotissue.js
+++ client/controllers/app/reprotissue.js
@@ -2,4 +2,8 @@ angular.module('app').controller('app_reprotissue', app_reprotissue);
 function app_reprotissue($scope, app) {
     'use strict';
     app.init($scope);
+    
+    $scope.save = function() {
+        app.action('reportissue', 'ReportIssue', this);
+    }
 }
